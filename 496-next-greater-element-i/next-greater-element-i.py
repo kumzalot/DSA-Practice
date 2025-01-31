8e5bcaca497261fdedbf394d5a1d57e1cada5c17
Barath Kumar <126680251+kumzalot@users.noreply.github.com>
2025-01-31 16:30:46 +0530

    Time: 11 ms (25.55%) | Memory: 18 MB (52.22%) - LeetSync

---
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -0,0 +1,18 @@
+class Solution:
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        res = [-1] * len(nums1)
+        index_map = {}
+        for i,j in enumerate(nums1):
+            index_map[j] = i
+        
+        stk = []
+        for i in range(len(nums2)-1, -1, -1):
+            while stk and nums2[stk[-1]] <= nums2[i]:
+                stk.pop()
+            
+            if stk and nums2[i] in nums1:
+                res[index_map[nums2[i]]] = nums2[stk[-1]]
+
+            stk.append(i)
+        
+        return res
